ef4916ed784ec1b9dfa36e55c439e4b3b4c1443f
wandanrfd <wandanurfadila01@gmail.com>
2026-09-25 21:09:19 +0700

    delete prisma neon

--- server/config/prisma.ts
+++ server/config/prisma.ts
@@ -1,9 +1,15 @@
+// import "dotenv/config";
+// import { PrismaNeon } from "@prisma/adapter-neon";
+
+// import { PrismaClient } from "../generated/prisma/index.js";
+
+// const adapter = new PrismaNeon({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// export const prisma = new PrismaClient({ adapter });
+
 import "dotenv/config";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "../generated/prisma/index.js";
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
